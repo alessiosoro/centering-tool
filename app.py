b11f9b50e633dd2f801202746d59559b8ef8bd18
alessiosoro <77458632+alessiosoro@users.noreply.github.com>
2025-04-02 09:40:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
 from backend.main import app
 
+if __name__ == "__main__":
+    import uvicorn
+    import os
+
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
+
+
